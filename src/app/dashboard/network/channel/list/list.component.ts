88cc62c279e2bc382591f3d4e16248202d625734
killsyre <killsyre@yahoo.com>
2022-03-07 21:12:46 +0100

    Added guide status Badge

--- src/app/dashboard/network/channel/list/list.component.ts
+++ src/app/dashboard/network/channel/list/list.component.ts
@@ -23,6 +23,7 @@ export class ListComponent implements OnInit {
     this.channels$ = this.route.data.pipe(
       map(data => {
         let cList = data.channelList;
+        // cList.forEach((item: Channel) => console.log(item))
         this.channels = cList;
         cList.map((channel: Channel) => {
           const f_date = new Date(<string>channel.created_at);
