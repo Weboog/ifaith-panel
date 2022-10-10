9f9549fe7c80c0b05491ed1897fa86e47c6c341e
Your Name <joebeurg@outlook.com>
2022-10-10 01:07:59 +0100

    Add Another Program

--- src/app/dashboard/guides/guide/guide.component.ts
+++ src/app/dashboard/guides/guide/guide.component.ts
@@ -1,6 +1,6 @@
 import { NetworkService } from './../../../services/network.service';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup } from '@angular/forms';
 import { Channel } from 'src/app/types/channel';
 
 @Component({
@@ -9,41 +9,71 @@ import { Channel } from 'src/app/types/channel';
   styleUrls: ['./guide.component.scss'],
 })
 export class GuideComponent implements OnInit {
-  @ViewChild('guide', { read: ElementRef })
-  guide!: ElementRef;
-  step: Number = 1;
+  @ViewChild('guideModel', { read: ElementRef })
+  guideEl!: ElementRef;
+  step: number = 0;
   filename: string = '';
   submitted: boolean = false;
-  list: Object[] = [];
+  channelList: { id: string; name: string; hash: string }[] = [];
+  guide!: { guide_title: string; channel: string; programs: Object[] };
+  // shakeBox: boolean = false;     // TODO: add to Form Model:  [ngClass]="{'shakeBox': shakeBox}"
 
   constructor(private network: NetworkService) {}
 
   ngOnInit(): void {
     this.network.list().subscribe((channels: Channel[]) => {
       channels.forEach((channel) => {
-        this.list.push({
+        this.channelList.push({
           id: channel.id,
           name: channel.channel,
           hash: channel.hash,
         });
       });
     });
+
   }
 
-  showProg(){
-    this.step = 0;
-    this.guide.nativeElement.style.display = 'none';
+  onNext(form: NgForm) {
+    let formObj = form.value.guideModel;
+    this.step++;
+    this.guideEl.nativeElement.style.display = 'none';
+
+    this.guide = {
+      guide_title: formObj.guide_title,
+      channel: formObj.channel,
+      programs: [],
+    };
   }
 
   onFileSelected(event: any) {
-    if (event.target.files.length > 0) {
+    if (event.target.files.length > 0)
       this.filename = event.target.files[0].name;
-    }
   }
 
-  resetFile(form: any, event: any) {
+  resetFile(form: NgForm, event: any) {
     if (event) event.preventDefault();
-    form.value.thumbnail = '';
+    this.setInvalid(form, 'thumbnail');
+    form.value.program.thumbnail = '';
+    this.filename = '';
+  }
+
+  onAdd(form: NgForm) {
+    this.step++;
+    // this.shakeBox = (form.valid && this.step !== 0) ? true : false;
+    const progs = form.value.program;
+
+    this.guide.programs.push({
+      program_title: progs.program_title,
+      air_datetime: progs.air_datetime,
+      thumbnail: progs.thumbnail,
+    });
+
+    ['program_title', 'air_datetime', 'thumbnail'].forEach((el) => {
+      const inp = document.getElementById(el) as HTMLInputElement;
+      inp.value = '';
+      this.setInvalid(form, el);
+    });
+
     this.filename = '';
   }
 
@@ -52,8 +82,22 @@ export class GuideComponent implements OnInit {
     history.back();
   }
 
+  setInvalid(form: NgForm, element: string) {
+    (form.form.get('program') as FormGroup).controls[element].setErrors({
+      invalid: true,
+    });
+  }
+
   onSubmit(form: NgForm) {
     this.submitted = true;
-    return form.value;
+    const progs = form.value.program;
+
+    this.guide.programs.push({
+      program_title: progs.program_title,
+      air_datetime: progs.air_datetime,
+      thumbnail: progs.thumbnail,
+    });
+
+    return this.guide;
   }
 }
